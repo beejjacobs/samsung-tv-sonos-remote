9abaa03cc59a258ca3a137e41256b5ca3d7b23eb
Ben Jacobs <ben.jacobs@vanti.co.uk>
2020-09-18 21:56:34 +0100

    dev: add font and icons

--- tv-remote/src/main.js
+++ tv-remote/src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '@mdi/font/css/materialdesignicons.min.css';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
